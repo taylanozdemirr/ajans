1ccde3cebb7612da96adb59cce486d077d5eb3ed
Taylan <taylan19.97a@gmail.com>
2026-06-24 11:14:12 +0300

    fix: env types for ci

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -1,2 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_USE_MOCK: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
